c6ee061efd86d8644e7995087877bc3d364124e6
Buletiss <vinicius1997feitosa@gmail.com>
2022-09-04 17:32:15 -0300

    up: sidebar

--- web/src/components/sidebar/index.jsx
+++ web/src/components/sidebar/index.jsx
@@ -1,31 +1,31 @@
-import { Box, Img, Link, List, ListItem } from '@chakra-ui/react';
+import { Flex, Img, Link } from '@chakra-ui/react';
 import perfilCirculo from '../../assets/perfilCirculo.png';
 import React from 'react';
-import { Link as ReachLink } from '@reach/router';
 
 export function Sidebar() {
   return (
-    <Box color="white" w="225px" h="100vh" bg="#1F5E81" p="20px" position="fixed">
-      <List>
-        <Img src={perfilCirculo} w="100px" margin="0px 25px 30px 40px" />
-        <ListItem padding="13px 20px">
-          <Link as={ReachLink} to="/cadastro">
-            Menu
+    <Flex color="white" w="225px" h="100vh" bg="#24344C">
+      <Flex
+        pos="sticky"
+        left="5"
+        h="95vh"
+        marginTop="2.5vh"
+        boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
+        borderRadius="30px"
+        w="200px"
+        flexDir="column"
+        justifyContent="space-between"
+      >
+        <Flex p="5%" flexDir="column" w="100%" alignItems="small" as="nav">
+          <Img src={perfilCirculo} boxSize="100px" ml="30px" mb="150px" />
+          <Link href="https://www.youtube.com/watch?v=BIyOsW4Fpzo" bg="black">
+            Teste
           </Link>
-        </ListItem>
-        <ListItem padding="13px 20px">
-          <Link>Pokemon1</Link>
-        </ListItem>
-        <ListItem padding="13px 20px">
-          <Link>Pokemon2</Link>
-        </ListItem>
-        <ListItem padding="13px 20px">
-          <Link>Pokemon5</Link>
-        </ListItem>
-        <ListItem padding="13px 20px">
-          <Link>Pokemon4</Link>
-        </ListItem>
-      </List>
-    </Box>
+          <Link href="https://www.youtube.com/watch?v=yzSFVOI8sIg&t=1448s">Teste1</Link>
+          <Link href="https://www.youtube.com/watch?v=bHGsP-keGFs">Teste2</Link>
+          <Link href="https://www.youtube.com/watch?v=jYdaQJzcAcw">Teste3</Link>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
