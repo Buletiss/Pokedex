b8efc1140281585d8a1f8c681342c620c5f8e878
Buletiss <vinicius1997feitosa@gmail.com>
2022-09-21 11:42:57 -0300

    add: nome dos pokemons em tela

--- web/src/components/sidebar/index.jsx
+++ web/src/components/sidebar/index.jsx
@@ -1,62 +1,28 @@
-import { Flex, Img, Link } from '@chakra-ui/react';
-import perfilCirculo from '../../assets/perfilCirculo.png';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Flex } from '@chakra-ui/react';
+import api from 'axios';
 
 export function Sidebar() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    async function personagens() {
+      const response = await api.get('http://localhost:3333/listarpokemon');
+
+      setPokemons(response.data);
+    }
+    personagens();
+  });
+
   return (
-    <Flex color="white" w="225px" h="100vh" bg="#24344C">
-      <Flex
-        pos="sticky"
-        left="5"
-        h="95vh"
-        marginTop="2.5vh"
-        w="200px"
-        flexDir="column"
-        justifyContent="space-between"
-      >
-        <Flex
-          p="5%"
-          flexDir="column"
-          w="100%"
-          alignItems="small"
-          as="nav"
-        >
-          <Img
-            src={perfilCirculo}
-            boxSize="100px"
-            ml="30px"
-            mb="150px"
-          />
-          <Link
-            href="https://www.youtube.com/watch?v=BIyOsW4Fpzo"
-            bg="black"
-            marginBottom="20px"
-          >
-            Teste
-          </Link>
-          <Link
-            marginBottom="20px"
-            bg="black"
-            href="https://www.youtube.com/watch?v=yzSFVOI8sIg&t=1448s"
-          >
-            Teste1
-          </Link>
-          <Link
-            marginBottom="20px"
-            bg="black"
-            href="https://www.youtube.com/watch?v=bHGsP-keGFs"
-          >
-            Teste2
-          </Link>
-          <Link
-            marginBottom="20px"
-            bg="black"
-            href="https://www.youtube.com/watch?v=jYdaQJzcAcw"
-          >
-            Teste3
-          </Link>
-        </Flex>
-      </Flex>
-    </Flex>
+    <>
+      <div className="pokemons-div">
+        {pokemons.map(pokemon => (
+          <div>
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
